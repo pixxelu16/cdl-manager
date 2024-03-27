44c1531cdd44a14b89cedb2e27cd7ff8f245ae07
sunilkapoor2444 <46962322+sunilkapoor2444@users.noreply.github.com>
2024-03-27 17:17:21 +0530

    create-api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Register
+Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+//Login
+Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+//Get customer users
+Route::get('all-customer-users-list', [App\Http\Controllers\Api\AuthController::class, 'get_user_profile']);
+//Update customer user
+Route::post('update-customer/{id}', [App\Http\Controllers\Api\AuthController::class, 'update_user_profile']);
+//Change customer user password
+Route::post('update-customer-user-password/{id}', [App\Http\Controllers\Api\AuthController::class, 'updated_password']);
+
+
