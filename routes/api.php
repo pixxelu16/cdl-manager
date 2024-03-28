c75521ff62466d948db7907c7c1c3f2d00227e94
sunilkapoor2444 <46962322+sunilkapoor2444@users.noreply.github.com>
2024-03-28 12:24:35 +0530

    api-controller-changes

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'register']);
 //Login
 Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login']);
-//Get customer users
-Route::get('all-customer-users-list', [App\Http\Controllers\Api\AuthController::class, 'get_user_profile']);
+//Get single user detail
+Route::get('get-single-user-detail', [App\Http\Controllers\Api\AuthController::class, 'get_single_user_profile']);
 //Update customer user
-Route::post('update-customer/{id}', [App\Http\Controllers\Api\AuthController::class, 'update_user_profile']);
+Route::post('update-user-profile', [App\Http\Controllers\Api\AuthController::class, 'update_user_profile']);
 //Change customer user password
-Route::post('update-customer-user-password/{id}', [App\Http\Controllers\Api\AuthController::class, 'updated_password']);
+Route::post('update-password', [App\Http\Controllers\Api\AuthController::class, 'updated_password']);
 
 
